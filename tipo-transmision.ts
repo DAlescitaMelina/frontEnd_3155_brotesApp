fd34473b616a46c4c494c9c429b1b5be8de8ed5f
BrendaLeveau1309 <135052218+BrendaLeveau1309@users.noreply.github.com>
2025-06-23 22:27:38 -0500

    Crud de Tipo de transmisión

--- tipo-transmision.ts
+++ tipo-transmision.ts
@@ -2,27 +2,41 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Subject } from 'rxjs';
+import { TipoTransmision } from '../models/tipoTransmision';
 const base_url=environment.base
 @Injectable({
   providedIn: 'root'
 })
 export class TipoTransmisionService {
   private url=`${base_url}/tiposT`
-  private listaCambio=new Subject<TipoTransmisionService[]>
-
+  private listaCambio=new Subject<TipoTransmision[]>
   constructor(private http:HttpClient) { }
 
-  list(){
-    return this.http.get<TipoTransmisionService[]>(this.url)
-  }
-   insert(a:TipoTransmisionService){
-    return this.http.post(this.url,a)
-  }
-
-  setList(listaNueva:TipoTransmisionService[]){
-    this.listaCambio.next(listaNueva)
-  }
-  getList(){
-    return this.listaCambio.asObservable()
-  }
+    list(){
+      return this.http.get<TipoTransmision[]>(this.url +"/lista")
+    }
+  
+    insert(ta:TipoTransmision){
+      return this.http.post(this.url+"/inserta",ta)
+    }
+  
+    setList(listaNueva:TipoTransmision[]){
+      this.listaCambio.next(listaNueva)
+    }
+  
+    getList(){
+      return this.listaCambio.asObservable()
+    }
+  
+    listId(id:number){
+      return this.http.get<TipoTransmision>(`${this.url}/${id}`)
+    }
+  
+    update(ta:TipoTransmision){
+      return this.http.put(this.url+"/modifica",ta)
+    }
+  
+    deleteA(id:number){
+      return this.http.delete(`${this.url}/${id}`)
+    }
 }
